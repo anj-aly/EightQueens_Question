66d92d264c13d708ae467586e89cb8e3f63e895a
HP <HP@LAPTOP-I4PSNO1R.mshome.net>
2021-07-10 11:31:13 +0530

    Fixed Functions

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -1,8 +1,10 @@
 public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
-
-		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
+        int row=square/8;        
+        int column=square%8;
+		if((row+column)%2==0) //To determine if the cell is black or white
+        	 return false;
+		//System.out.println(square);
 		return true;
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -3,15 +3,12 @@ public class GameStrategy {
 	private int numQueens = 0;
 
 	private int getColumn(int cellId) {
-		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		
+		return (cellId)%8;	//To get the column of given cell
 	}
 	
 	private int getRow(int cellId) {
-		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+	    return cellId/8;	// To get the row of given cell
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -25,10 +22,31 @@ public class GameStrategy {
 		
 		int row = getRow(cellId);
 		
-		/*
-			WRITE YOUR LOGIC HERE...............................
-
-		*/
+		
+		int i,j;
+		for(i=0;i<8;i++)//To check if the column is free
+			if(placedQueens[i][col]==true)
+				isValid=false;
+		for(i=0;i<8;i++) //To check if the row is free
+			if(placedQueens[row][i]==true)
+				isValid=false;
+		for(i=row,j=col;i>=0&&j>=0;i--,j--)//To check if upper primary diagonal is free
+			if(placedQueens[i][j]==true)
+				isValid=false;
+		for(i=row,j=col;i<8&&j<8;i++,j++)//To check if lower primary diagonal is free
+			if(placedQueens[i][j]==true)
+				isValid=false;
+		for(i=row,j=col;j>=0&&i<8;i++,j--)//To check if upper secondary diagonal is free
+			if(placedQueens[i][j]==true)
+				isValid=false;
+		for(i=row,j=col;j<8&&i>=0;i--,j++)//To check if lower secondary diagonal is free
+			if(placedQueens[i][j]==true)
+				isValid=false;
+		if(isValid) {                    //If its safe place the queen update placedQueens and numQueens
+			placedQueens[row][col]=true;
+			numQueens++;
+		}
+		
 		return isValid;
 	}
 }
